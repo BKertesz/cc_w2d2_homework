ea510ab23ca1a989a15d2e6ab9d08e93a6f485f3
BKertesz <kertesz.balazs92@gmail.com>
2018-03-13 17:16:37 +0000

    Added river class & test

--- river.rb
+++ river.rb
@@ -1,1 +1,15 @@
+class River
+  def initialize(name,fishes)
+    @name = name
+    @fishes = fishes
+  end
 
+  def fish_count()
+    return @fishes.length
+  end
+
+  def a_fish_is_born(type)
+    fish = Fish.new(type)
+    @fishes << fish
+  end
+end
--- specs/river_specs.rb
+++ specs/river_specs.rb
@@ -5,6 +5,18 @@ require_relative "../fish"
 class RiverTest < Minitest::Test
 
   def setup
+    fish1 = Fish.new("Salmon")
+    fish2 = Fish.new("Trout")
+    @river = River.new("Amazon",[fish1,fish2])
+  end
+
+  def test_fish_count
+    assert_equal(2,@river.fish_count)
+  end
+
+  def test_a_fish_is_born()
+    @river.a_fish_is_born("Clown")
+    assert_equal(3,@river.fish_count)
   end
 
 
